462d7bdb427468d15f552ac147ebfb6bf795a660
rodionros <r.rostovshchikov@yandex.ru>
2024-12-28 17:01:59 +0300

    feat: Company module

--- src/main.ts
+++ src/main.ts
@@ -12,11 +12,7 @@ import { setupSwagger } from './swagger';
 async function bootstrap() {
     const logger = new Logger();
     const app = await NestFactory.create(AppModule, new ExpressAdapter(express()), {
-        cors: {
-            origin: ['http://localhost:8888'],
-            methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-            allowedHeaders: 'Content-Type,Authorization',
-        },
+        cors: false,
     });
 
     const configService = app.get(ConfigService);
